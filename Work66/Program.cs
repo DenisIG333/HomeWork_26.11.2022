13557e660fc67cc874a43bfcf60290dfd9f61d8f
DenisIG333 <ignatovdnc@inbox.ru>
2022-11-30 16:31:18 +0300

    Home Work 26.11.22

---
+++ Work66/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт
+сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+ */
+
+
+int m = UserNumber("Введите M:");
+int n = UserNumber("Введите N:");
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {m} до {n} = {NaturalSum(m, n)}");
+
+int UserNumber(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int NaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + NaturalSum(m, n - 1);
+}
